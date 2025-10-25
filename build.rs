6144506890f9ee6263d0fb016f5ba8dea327017e
zhch <zhchqingdao@163.com>
2025-10-26 00:10:12 +0800

    build task bar icon

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("ui/assets/icon.ico");
+    res.compile().expect("Failed to compile resources");
 }
